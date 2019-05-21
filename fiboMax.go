89b1f3983dffc1ab5b2b553d2d9fcb6d155fb5ae
Max Ahlberg <maxahlberg@hotmail.com>
2019-05-21 15:03:47 +0200

    Include recursive solution to the fibonacci sequence Adding a body here...

--- fiboMax.go
+++ fiboMax.go
@@ -6,12 +6,21 @@ func fib(input int8) (resulting int8) {
 	v0 := int8(0)
 	v1 := int8(1)
 
-	for i := int8(0); i < input; i++ {
+	for i := int8(1); i < input; i++ {
 		resulting = v0 + v1
 		v0 = v1
 		v1 = resulting
 	}
 	return resulting
+}
+
+
+func fibRec(input int8) int8{
+	if input == 0 || input == 1{
+	return input
+	}
+	return fibRec(input -2) + fibRec(input -1)
+
 }
 
 func main() {
@@ -21,8 +30,11 @@ func main() {
 	fmt.Println("The input was: ", input)
 
 	var output int8
-
+	var output2 int8
 	output = fib(input)
+	output2 = fibRec(input)
+
 	fmt.Println("The fibonatchi number", input, "is", output)
+	fmt.Println("According to the recursive method it is: ", output2)
 
 }
